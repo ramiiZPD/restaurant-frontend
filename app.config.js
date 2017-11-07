f0672180f9c7c9e8e95e4bda9b185b272b4036c6
Ramitha <ramithakalana@gmail.com>
2017-11-07 17:20:13 +0530

    first commit

---
+++ app.config.js
@@ -0,0 +1,12 @@
+angular.
+module('scotchApp').
+config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+
+        $routeProvider.
+        when('/home', {
+            template: '<restaurant-ui></restaurant-ui>'
+        }).
+        otherwise('/#!/home');
+    }
+]);
---
+++ restaurant-ui/restaurant-ui.component.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    angular.
+    module('restaurantUi').
+    component('restaurantUi', {
+        templateUrl: 'restaurant-ui/restaurant-ui.template.html',
+        controller: function restaurantuiController($http, $scope) {
+
+            // restaurantuiController.inject = ['$http', '$scope'];
+
+        }
+    });
+})();
