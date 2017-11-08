b87902e5b0457b807b8231238504d82b0c388599
Ramitha <ramithakalana@gmail.com>
2017-11-08 17:04:27 +0530

    reservation completed

--- restaurant-ui/restaurant-ui.component.js
+++ restaurant-ui/restaurant-ui.component.js
@@ -6,8 +6,42 @@
         templateUrl: 'restaurant-ui/restaurant-ui.template.html',
         controller: function restaurantuiController($http, $scope) {
 
-            // restaurantuiController.inject = ['$http', '$scope'];
+            restaurantuiController.inject = ['$http', '$scope'];
 
+            var vm = this;
+
+            vm.makeNewReservation = makeNewReservation;
+
+            // function makeNewReservation($scope) {
+            //     var jsonHeaderObject={headers:{'Content-Type': 'application/json'}}
+            //
+            //
+            //     $http({
+            //         url: 'http://localhost:8080/api/reservations',
+            //         method: "POST",
+            //         data: {"reservation_id" : vm.reservationID, "user_id" : vm.userID, "mobile" : vm.mobileNumber, "no_of_guests" : vm.noGuests, "arrival_time" : vm.arivalTime, "email" : vm.email
+            //         }
+            //     })
+            //         .then(function(response) {
+            //             console.log("Success");
+            //             },
+            //             function(response) { // optional
+            //                 console.log("failed");
+            //             });
+            // }
+
+            function makeNewReservation () {
+                var jsonHeaderObject={headers:{'Content-Type': 'application/json'}}
+                console.log("Working!")
+                var data ={"reservation_id" : vm.reservationID, "user_id" : vm.userID, "mobile" : vm.mobileNumber, "no_of_guests" : vm.noGuests, "arrival_time" : vm.arivalTime, "email" : vm.email
+                };
+
+                $http.post('http://localhost:8080/api/reservations', data, jsonHeaderObject).then(function (response) {
+                    console.log("Successfully send!")
+                },function (response) {
+                    console.log("failed!");
+                });
+            }
         }
     });
 })();
